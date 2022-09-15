bb3b3ce14a22f60117f0c13c9723ff42b3650607
baranau_an <andrei.baranau@sp.woodplc.com>
2022-09-15 13:53:43 +0300

    Lab 10: Asynchronously process messages by using Azure Service Bus Queues

---
+++ Allfiles/Labs/10/Starter/MessagePublisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace MessagePublisher
+{
+    public class Program
+    {
+        private const string storageConnectionString = "place_your_sb_connection_string_here";
+        private const string queueName = "messagequeue";
+        private const int numOfMessages = 3;
+        static ServiceBusClient client;
+        static ServiceBusSender sender;
+
+        public static async Task Main(string[] args)
+        {
+            client = new ServiceBusClient(storageConnectionString);
+            sender = client.CreateSender(queueName);
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+            
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
---
+++ Allfiles/Labs/10/Starter/MessageReader/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.ServiceBus;
+namespace MessageReader
+{
+    public class Program
+    {
+        private const string storageConnectionString = "place_your_sb_connection_string_here";
+        static string queueName = "messagequeue";
+        static ServiceBusClient client = default!;
+        static ServiceBusProcessor processor = default!;
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+
+        static async Task Main(string[] args)
+        {
+            client = new ServiceBusClient(storageConnectionString);
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                processor.ProcessMessageAsync += MessageHandler;
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                await processor.StartProcessingAsync();
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                await processor.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
